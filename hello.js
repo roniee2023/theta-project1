4f97914700299f50d9462bad315fed3d545ef539
binoy <binoykdasgupta@gmail.com>
2023-05-01 18:29:49 +0100

    multiple function added

--- hello.js
+++ hello.js
@@ -1,5 +1,11 @@
 console.log(' hello');
 
-let a =5, b =5;
-let c= a+b;
-console.log( c);
+let a = 5, b = 5;
+let c = a + b;
+console.log(c);
+
+const sub = function (x, y) {
+  return x * y;
+}
+
+console.log(`The multiplication of ${5} and ${3} give me ${sub(5, 3)}`);
